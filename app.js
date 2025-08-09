bc500156e5335bec62e08979cfcb2a950c14ab15
zigzaga00 <161963582+zigzaga00@users.noreply.github.com>
2025-08-09 20:45:00 +0100

    new deployment

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const navBtn = document.getElementById("nav-btn");
 const navbar = document.getElementById("navbar");
 const navClose = document.getElementById("nav-close");
+
 // show nav
 navBtn.addEventListener("click", () => {
   navbar.classList.add("showNav");
@@ -9,4 +10,11 @@ navBtn.addEventListener("click", () => {
 // close nav
 navClose.addEventListener("click", () => {
   navbar.classList.remove("showNav");
+});
+// close navbar
+const closeNavBar = () => navbar.classList.remove("showNav");
+
+// all links in navbar
+document.querySelectorAll("#navbar a").forEach((link) => {
+  link.addEventListener("click", closeNavBar);
 });
